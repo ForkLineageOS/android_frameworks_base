0844c68b806e44d81824055947d4f511ed50cde1
Alexander Martinz <alex@amartinz.at>
2017-08-25 12:01:11 +0200

    BatteryService: Add support for oem fast charger detection
    Allows to indicate, whether a device is charged using a
    proprietary oem fast charge solution, which write their status
    to sysfs.
    
    The oem fast charge detection tries to be as generic as possible and is
    configured via overlays.
    
    Path to sysfs to read status
      - core/res/res/values/lineage_config.xml
        - config_oemFastChargerStatusPath
    Value expected from read status (Defaults to "1" if not specified)
      - core/res/res/values/lineage_config.xml
        - config_oemFastChargerStatusValue
    
    Change-Id: I6f3598a5a6a3efc76553261d2cf73094170d4110
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- packages/SystemUI/src/com/android/keyguard/KeyguardUpdateMonitor.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardUpdateMonitor.java
@@ -1753,7 +1753,7 @@ public class KeyguardUpdateMonitor implements TrustManager.TrustListener, Dumpab
         }
 
         // Take a guess at initial SIM state, battery status and PLMN until we get an update
-        mBatteryStatus = new BatteryStatus(BATTERY_STATUS_UNKNOWN, 100, 0, 0, 0, 0, 0, 0, false, true);
+        mBatteryStatus = new BatteryStatus(BATTERY_STATUS_UNKNOWN, 100, 0, 0, 0, 0, 0, 0, false, true, false);
 
         // Watch for interesting updates
         final IntentFilter filter = new IntentFilter();
@@ -2682,6 +2682,11 @@ public class KeyguardUpdateMonitor implements TrustManager.TrustListener, Dumpab
 
         // change in VOOC charging while plugged in
         if (nowPluggedIn && current.voocChargeStatus != old.voocChargeStatus) {
+            return true;
+        }
+
+        // change in oem fast charging while plugged in
+        if (nowPluggedIn && current.oemFastChargeStatus != old.oemFastChargeStatus) {
             return true;
         }
 
